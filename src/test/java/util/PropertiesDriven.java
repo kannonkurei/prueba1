265746da75e29a2275a76fe94a9a2707f25f9903
efvinarta <ext_efvinarta@falabella.cl>
2022-10-06 15:17:34 -0300

    se agrega carpeta util al repo

---
+++ src/test/java/util/PropertiesDriven.java
@@ -0,0 +1,28 @@
+package util;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertiesDriven {
+	//Atributo
+	private static Properties prop;
+
+	public static String obtenerProperty(String key) {
+		prop = new Properties();
+		String rutaFile = System.getProperty("user.dir")+"\\src\\main\\resources\\properties.properties";
+		try {
+			InputStream input = new FileInputStream(rutaFile);
+			
+			prop.load(input);
+			
+		} catch (Exception e) {
+			System.out.println("No fue posible encontrar el archivo properties.properties");
+			System.out.println("Ruta definida para el archivo: "+rutaFile);
+			e.printStackTrace();
+		}
+		return prop.getProperty(key);
+		
+	}
+}
